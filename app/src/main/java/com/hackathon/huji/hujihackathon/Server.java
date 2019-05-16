b65540a41f77fa255295b7c3c4f254350fbab7ec
imri <imri.paran@mail.huji.ac.il>
2019-05-17 02:19:59 +0300

    added body

--- app/src/main/java/com/hackathon/huji/hujihackathon/Server.java
+++ app/src/main/java/com/hackathon/huji/hujihackathon/Server.java
@@ -2,12 +2,23 @@ package com.hackathon.huji.hujihackathon;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
+import android.util.Log;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Server {
     private static final Server ourInstance = new Server();
+    private static final String LOG_TAG = "Server";
 
     public static Server getInstance() {
         return ourInstance;
@@ -44,9 +55,53 @@ public class Server {
         groupsContainingUser.setValue(containingUser);
     }
 
-    public void fetchSuggestedGroups(Group group) {
+    public void fetchSuggestedGroups(Group group, Context context) {
+        final JSONObject body = new JSONObject();
+        try {
+            body.put("id", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = "http://www.google.com";
+        final List<Group> suggested = new ArrayList<>();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonGroups = new JSONArray(response);
+                            Gson gson = new Gson();
+
+                            for (int i = 0; i < jsonGroups.length(); i++) {
+                                Group group = gson.fromJson(jsonGroups.getJSONObject(i).toString(), Group.class);
+                                suggested.add(group);
+                            }
+                        } catch (JSONException e) {
+                            Log.d(LOG_TAG, Arrays.toString(e.getStackTrace()));
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError e) {
+                Log.d(LOG_TAG, Arrays.toString(e.getStackTrace()));
+            }
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return body.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        queue.add(stringRequest);
+
         // should call remote api and get the list
-        List<Group> suggested = new ArrayList<>();
         Group avengers = new Group("Avengers", String.valueOf(++id), "https://ichef.bbci.co.uk/news/624/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg", "hackathon", "huji");
         avengers.addMember(new User("Tal"));
         avengers.addMember(new User("Itay"));
