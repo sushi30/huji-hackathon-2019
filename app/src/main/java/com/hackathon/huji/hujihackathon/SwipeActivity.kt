ba5cf197dc80bbb22f35dfbbf5c7d794695a2dc2
imri <imri.paran@mail.huji.ac.il>
2019-05-16 12:00:13 +0300

    swipe initial

---
+++ app/src/main/java/com/hackathon/huji/hujihackathon/SwipeActivity.kt
@@ -0,0 +1,12 @@
+package com.hackathon.huji.hujihackathon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SwipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe)
+    }
+}
